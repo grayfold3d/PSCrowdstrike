73791d60604a105ca4aa19bb6b77bdd412dd5dd5
Jonathan Moss <jmoss@Jonathans-MacBook-Air.local>
2019-07-30 18:38:18 -0400

    upload

---
+++ PSCrowdstrike/Public/Get-CSDomainMachines.ps1
@@ -0,0 +1,38 @@
+function Get-CSDomainMachines {
+    <#
+    .SYNOPSIS
+        Function to retrieve host info from Crowdstrike via /devices/queries/devices/v1 and
+        /devices/entities/devices/v1 endpoint.
+    .DESCRIPTION
+        This function provides a way to retrieve using common parameters required by PSCrowdstrike.
+    .EXAMPLE
+        PS C:\> Get-CSDomainMachines -Domain "domain.local"
+        Retrieves all AgentID's in Crowdstrike for Servers in domain.local
+    .INPUTS
+        None
+    .PARAMETER Domain
+        The hostname you would like to query.
+    .OUTPUTS
+        None
+    .NOTES
+        None
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Domain
+    )
+
+    begin {
+        $DeviceEndpoint = "/devices/queries/devices/v1?filter=machine_domain%3A$domain"
+    }
+
+    process {
+        (Invoke-CSRestMethod -Endpoint $DeviceEndpoint -Method "GET").Resources
+    }
+
+    end {
+        $Output
+    }
+}
